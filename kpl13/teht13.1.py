3a07e77b0720eb3d7f6da4a46103caa1930d05d6
haris <harisdzino2004@gmail.com>
2024-11-22 00:54:54 +0200

    g

--- kpl13/teht13.1.py
+++ kpl13/teht13.1.py
@@ -1,14 +1,62 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
-@app.route('/alkuluku/<luku>)
+@app.route('/alkuluku/<luku>')
 def alkuluku(luku):
-    x = int(luku)
-    y = 2
-    while x>y:
-        if x%y== 0
-            z=false
-            break
-        else:y=y+1
-    if x ==y:
-        z=vastaus
+    try:
+        x = int(luku)
+        if x <= 1:
+            z = False
+        else:
+            z = True
+            y = 2
+            while y * y <= x:
+                if x % y == 0:
+                    z = False
+                    break
+                y += 1
+
+        return jsonify({
+            "Number": x,
+            "isPrime": z
+        })
+    except ValueError:
+        return jsonify({"error": "invalid input!"}), 400
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+#def onko_alkuluku(n):
+  #  if n <= 1:
+  #      return False
+ #   for i in range(2, int(n**0.5) + 1):
+ #       if n % i == 0:
+   #         return False
+#    return True
+
+# Flask-reitti
+#@app.route('/alkuluku/<int:luku>', methods=['GET'])
+#def tarkista_alkuluku(luku):
+  #  tulos = {
+   #     "Number": luku,
+    #    "isPrime": onko_alkuluku(luku)
+  #  }
+ #   return jsonify(tulos)
+
+# Sovelluksen käynnistäminen
+#if __name__ == '__main__':
+   # app.run(debug=True, port=3000)
+  #      from flask import Flask, request
+
+    #    app = Flask(__name__)
+
+      #  @app.route('/summa')
+    #    def summa():
+        #    args = request.args
+        #    luku1 = float(args.get("luku1"))
+        #    luku2 = float(args.get("luku2"))
+        #    summa = luku1 + luku2
+       #     return str(summa)
+
+     #   if __name__ == '__main__':
+      #      app.run(use_reloader=True, host='127.0.0.1', port=3000)
