6430230a1f820ec60e066cf42fd00985c9c43365
haris <harisdzino2004@gmail.com>
2024-11-29 03:36:19 +0200

    g

--- kpl4javahtml/teht4.2.js
+++ kpl4javahtml/teht4.2.js
@@ -1,26 +1,18 @@
-
 document.getElementById('searchForm').addEventListener('submit', function(event) {
-
   event.preventDefault();
 
-
   const query = document.getElementById('query').value;
-
-
   if (!query) {
     console.error('Please enter a search query');
     return;
   }
 
-
   fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
     .then(response => response.json())
     .then(data => {
-
       console.log('Search Results:', data);
     })
-    .catch(error => {
 
-      console.error('There was a problem with the fetch operation:', error);
-    });
 });
+
+// luulen että toimii, mutta en ole varma?
